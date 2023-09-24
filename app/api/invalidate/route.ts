e604f4384c6b01d340a06712d62c4c7ed592a6c5
cameronjpr <cameronjpr@gmail.com>
2023-09-24 11:28:34 +0100

    revalidate root path

--- app/api/invalidate/route.ts
+++ app/api/invalidate/route.ts
@@ -2,7 +2,7 @@ import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export function POST() {
-  revalidatePath('/posts');
+  revalidatePath('/');
 
   return NextResponse.json({
     ok: true,
