e104f6a5aab01ade26045b626d539f2b3815183b
cameronjpr <cameronjpr@gmail.com>
2023-09-24 11:26:25 +0100

    add res

--- app/api/invalidate/route.ts
+++ app/api/invalidate/route.ts
@@ -1,5 +1,10 @@
 import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
 
 export function POST() {
   revalidatePath('/posts');
+
+  return NextResponse.json({
+    ok: true,
+  })
 } 
